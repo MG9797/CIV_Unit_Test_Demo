475131c774b9276efca68614287e85934633a739
MG9797 <102201178+MG9797@users.noreply.github.com>
2022-05-24 22:09:03 +1000

    Add files via upload

--- UnitTestDemoTests/BollocksTests.cs
+++ UnitTestDemoTests/BollocksTests.cs
@@ -16,17 +16,18 @@ namespace UnitTestDemoTests
         }
 
         [Theory]
-        [InlineData(25, 5, 5)]
-        public void AddToTest()
+        [InlineData(5, 5, 10)]
+        public void AddToTest(int input,int bollocks, int expected)
         {
-            Assert.True(false);
+            Bollocks b = new Bollocks(bollocks, "bollocks1");
+            Assert.Equal(expected , b.AddTo(input));
         }
 
         [Theory]
         [InlineData(25, 5, 5)]
         [InlineData(2, 2, 1)]
         [InlineData(10, 3, 3)]
-         public void DivByTest(int input, int divisor, int expected)
+        public void DivByTest(int input, int divisor, int expected)
         {
             Bollocks b = new Bollocks(input, "bollocks2");
             int result = b.DivBy(divisor);
@@ -34,16 +35,26 @@ namespace UnitTestDemoTests
         }
         
         [Fact]
-         public void DivBy_Zero_Test()
+        public void DivBy_Zero_Test()
         {
-            Assert.True(false);
+            Bollocks b = new Bollocks(0, "bollocks1");
+            try
+            {
+                b.DivBy(0);
+                Assert.True(false);
+            }
+            catch
+            {
+                Assert.True(true);
+            }
         }
 
         [Theory]
-        [InlineData(25, 5, 5)]
-        public void ModOfDivByTest()
+        [InlineData(11, 2, 1)]
+        public void ModOfDivByTest(int input, int mod, int expected)
         {
-            Assert.True(false);
+            Bollocks b = new Bollocks(input, "bollocks1");
+            Assert.Equal(expected, b.ModOfDivBy(mod));
         }
     }
 }
